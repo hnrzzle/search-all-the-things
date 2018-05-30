a035096379c17f4055eb9ca4bea9bbd6bcbed038
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-05-30 16:54:40 -0700

    yay its working

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,15 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
+  render() {
 
+
+    return(
+    <main>
+      <h1>Is this working</h1>
+    </main>
+    );
+  }
 
 
 
