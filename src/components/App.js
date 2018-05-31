ac3f62ccc8ac1660b895c29d3c17efb6210409be
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-05-31 09:17:27 -0700

    starting search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { search } from '../services/marvelApi';
 
 
 
 export default class App extends Component {
 
   state = {
-    name: '',
-    
+    topic: '',
+    loading: false,
+    error: null,
+    totalResults: 0,
+    page: 1,
+    perPage: 20,
+    characters: []
   }
+
+  searchCharacters = () => {
+    const { topic, page, perPage } = this.state;
+
+
+    search({ topic }, { page, perPage })
+      .then(({ articles, totalResults }) => {
+        this.setState({ articles, totalResults, error: null });
+      }, error => {
+        this.setState({ error });
+      })
+      .then(() => this.setState({ loading: false }));
+
+  };
 
   render() {
 
--- src/services/marvelApi.js
+++ src/services/marvelApi.js
@@ -8,18 +8,15 @@ const HASH_QUERY = `&hash=${HASH}`;
 const EVERYTHING_URL = `${BASE_URL}${TS_QUERY}${API_QUERY}${HASH_QUERY}`
 
 
-// const throwJson = json => { throw json; };
-// const get = url => fetch(url)
-//   .then(r => r.ok ? r.json() : r.json().then(throwJson));
+const throwJson = json => { throw json; };
+const get = url => fetch(url)
+  .then(r => r.ok ? r.json() : r.json().then(throwJson));
 
-function search({ topic }, page = 1, pageSize = 20 ) {
+function search({ topic }, { page = 1, perPage = 20 }) {
   const search = `&nameStartsWith=${topic}`;
-  const paging = `&offset=${page * pageSize}&limit=${pageSize}`;
+  const paging = `&offset=${page * perPage}&limit=${perPage}`;
 
-  return `${EVERYTHING_URL}${search}${paging}`;
+  return get(`${EVERYTHING_URL}${search}${paging}`);
 }
 
-console.log(search({ topic: 'a' }));
-
-
-'http://gateway.marvel.com/v1/public/characters?nameStartsWith=a&ts=1&apikey=93f45297a2883d101c9b09a662ee1754&hash=7146c279264ea35e0bfb7ea39dfe57c0'
+// proper url format http://gateway.marvel.com/v1/public/characters?nameStartsWith=a&ts=1&apikey=93f45297a2883d101c9b09a662ee1754&hash=7146c279264ea35e0bfb7ea39dfe57c0
