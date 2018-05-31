6130a64f11b184cb095bebd30174e3424f7d4e75
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-05-31 11:40:06 -0700

    add image thumbnails

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 export default class Character extends Component {
 
   render() {
-    const { name, description } = this.props.character;
+    const { name, description, thumbnail } = this.props.character;
+
+    const imgSize = '/portrait_fantastic';
+    const { path, extension } = thumbnail;
+
+    const imgLink = `${path}${imgSize}.${extension}`
 
     return(
       <li>
         <h2>{name}</h2>
+        <img src={imgLink}/>
+        <p>{description}</p>
       </li>
     );
   }
