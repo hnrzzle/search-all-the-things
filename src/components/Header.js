f0d3f8217380b285944b8887d00944f621b0b527
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-04 17:40:04 -0700

    css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,6 +16,14 @@ export default class Header extends Component {
         <div className="header-container">
           <h1>Marvel Heroes</h1>
         </div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/search">Search</Link></li>
+          <li><Link to="/about">About</Link></li>
+
+
+
+        </ul>
       </header>
     );
   }
